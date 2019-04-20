c8e956a5e0859a5e29055e2a6b4661d4094ba4dc
yaojia1 <288167@supinfo.com>
2019-04-20 09:32:14 +0800

    add

--- elements.py
+++ elements.py
@@ -46,7 +46,7 @@ class Brick(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         if putimage=="brown":
             self.life=5
-        else:self.life=2
+        else:self.life=1
     def fall(self,tics):
         if self.color == "crystal" :
             if self.stoptime > 0:
@@ -56,25 +56,31 @@ class Brick(pygame.sprite.Sprite):
                 print("透明色消除")
                 drillbrick(self)
                 return 0
-        if self.rect.top>=2200-dril1.level:#到底了
+        #if self.rect.top>=(200+map1.num*1000-dril1.level):#到底了
+        if (self.rect.top+dril1.level) >= (map1.length+199):  # 行数，>=
             #self.sta=1
-            self.fuck=1
-            if self.mel!=1 and self.mel!=0:
-                for lala in self.mel.mapn:
-                    #lala.sta=1
-                    lala.fuck=1
-            return 0
-        if self.fuck==1:return 0 #到底了
-        if self.mel:
+            '''
+            self.fuck=1'''
+            if  self.mel!=0:#self.mel!=1 and
+                if self.fuck==2:return 0
+                else:#只定义一次组参数fuck
+                    self.fuck = 2
+                    if self.mel == 1:
+                        for grrr in meltgroup:
+                            if pygame.sprite.Group.has(grrr.mapn, self):
+                                grrr.fuck = 1
+                                return 0
+                    else:
+                        self.mel.fuck = 1
+                        return 0
+            return 0#单个的
+        #if self.fuck==1:return 0 #到底了
+        if self.mel:#if 融合了
             if self.mel == 1:
-                if self.fuck:
-                    for grrr in meltgroup:
-                        if pygame.sprite.Group.has(grrr.mapn, self):
-                            for bbb in grrr.mapn:bbb.fuck=1
-                            return 0
                 return 0
             else:
                 gg = self.mel  # 找到尊在的那个组
+            if gg.fuck==1:return 0
             if gg.fallable==0:#正在下落不能打扰
                 kll=0
                 gg.statu+=1
@@ -88,32 +94,17 @@ class Brick(pygame.sprite.Sprite):
                             for br in map1.brickGroup:
                                 br.rect.top += dril1.level
                             dril1.level = 0
+                    '''sta是否移动过'''
                     sss.sta+=1
                     sss.rect.top += 2
                 if (sss.rect.top+dril1.level) % 50 == 0:
                     kll = 1         # 下落到50倍数了
                 if kll:
-                    '''失败尝试
-                    for sss in gg.mapn:
-                        x=sss.rect.left//50
-                        y=sss.rect.top//50
-                        if y==0:continue
-                        if BRICKG[y-1][x]==sss:
-                            BRICKG[y - 1][x] = 0
-                        BRICKG[y][x] = sss'''
                     gg.fallable=1#可判断是否下咯
                 return 0
             else:
                 fall123=1#融合在fall
                 for spr in gg.mapn:  # 组里的每一个石块
-                    '''原方案：
-                    if spr.rect.top+25 < self.rect.top and spr.rect.left==self.rect.left: continue
-                    test_list = pygame.sprite.spritecollide(spr, map1.brickGroup, False)  # spr相交的石块
-                    for ll in test_list:  # 查看有没有托
-                        if pygame.sprite.Group.has(gg.mapn, ll):
-                            continue
-                        if ll.rect.top >= spr.rect.top+50 and ll.rect.left == spr.rect.left:  # 掉不下去
-                            return 0'''
                     '''第二次尝试'''
                     if spr.rect.top + 25 < self.rect.top and spr.rect.left == self.rect.left: continue
                     x=spr.rect.left//50
@@ -123,13 +114,6 @@ class Brick(pygame.sprite.Sprite):
                             continue
                         if ll.rect.top >= spr.rect.top + 50 and ll.rect.left == spr.rect.left:  # 掉不下去
                             return 0
-                    '''失败尝试
-                    x=spr.rect.left//50
-                    y = spr.rect.top//50
-                    if y >= 39:continue
-                    if BRICKG[y+1][x]:
-                        if BRICKG[y+1][x].color == spr.color:continue
-                        else:return 0#掉不下去'''
                 for sss in gg.mapn:
                     sss.rect.top += 2
                 gg.fallable = 0#没找到托，正在下落
@@ -145,29 +129,6 @@ class Brick(pygame.sprite.Sprite):
                     self.melt()
                     self.fallingsingle = 1
                     return 0
-        '''原单个、压住了:
-        collide_list = pygame.sprite.spritecollide(self, map1.brickGroup, False)
-        for sp in collide_list:
-            if  self.rect.left==sp.rect.left:
-                if sp.rect.top == self.rect.top + 50:#压住了
-                    self.melt()
-                    self.fallingsingle=1
-                    return 0'''
-        '''失败尝试。单个是否压住
-        x = self.rect.left // 50
-        y = self.rect.top // 50
-        if self.rect.top%50==0 and self.fallingsingle==0:
-            
-            if y >= 39: return 0
-            if BRICKG[y + 1][x]:
-                BRICKG[y][x]=self
-                if y==0:pass
-                else:
-                    if BRICKG[y-1][x]==self:
-                        BRICKG[y - 1][x] =0
-                self.melt()
-                self.fallingsingle = 1
-                return 0'''
         if self.stoptime==0:
             if self.rect.top + 5 <= dril1.rect.top <= self.rect.top + 50:
                 if self.rect.left <= dril1.rect.left + 16 <= self.rect.left + 50:
@@ -179,6 +140,7 @@ class Brick(pygame.sprite.Sprite):
                     dril1.level = 0
             self.rect.top += 2
             self.fallingsingle=0
+            '''sta'''
             self.sta +=1
     def melt(self):
         if self.color=='white':
@@ -198,7 +160,7 @@ class Brick(pygame.sprite.Sprite):
                             self.mel.aadd(sp)
                             sp.mel = 1
                         else:
-                            self.mel = Mapnn(self)  # 一个对象
+                            self.mel = Meltgroupadd(self)  # 一个对象
                             sp.mel = 1
                             self.mel.aadd(sp)
                             meltgroup.append(self.mel)
@@ -206,9 +168,10 @@ class Brick(pygame.sprite.Sprite):
     def update(self,tics):
         self.fall(tics)
 
-class Mapnn():
+class Meltgroupadd():
     stopp=1
     fallable=1
+    fuck=0
     def __init__(self,ga):
         self.mapn = pygame.sprite.Group()
         self.mapn.add(ga)
@@ -222,26 +185,57 @@ class Mapnn():
         self.statu+=gb.sta
 
 class Map():
+  num=1
   def __init__(self):
       # 初始化砖块群组
       self.brickGroup=pygame.sprite.Group()
-
-      XY = [(x,y) for x in range(11) for y in range(40)]#53，206
+      h=10
+      self.length=h*50
+      XY = [(x,y) for x in range(11) for y in range(h)]#53，206
       '''第二次尝试'''
       self.line=[]
-      for i in range(11):
+      for i in range(11):#生成列组
           nn=pygame.sprite.Group()
           self.line.append(nn)
+      ss=0
       for x,y in XY:
               # 实例化砖块类对象
               self.brick=Brick(random.choice(['red','blue','green','yellow','brown','white','crystal','air']))
               # 生砖块的位置
-              self.brick.rect.left,self.brick.rect.top=3+x*50,250+y*50# 每循环一次自动将动画添加到精灵组（下同）
+              self.brick.rect.left,self.brick.rect.top=3+x*50,250+y*50# 每循环一次自动将动画添加到精灵组（下同）上面空出250
               self.brickGroup.add(self.brick)
               self.line[x].add(self.brick)
+              ss+=1
+  def changemap(self):
+      meltgroup[:]=[]
+      self.num =1
+      self.brickGroup.empty()
+      # self.brickGroup = pygame.sprite.Group()
+      h=dril1.levelwin*5+5
+      self.length=h*50
+      XY = [(x, y) for x in range(11) for y in range(h)]  # 53，206
+      '''第二次尝试'''
+      for i in range(11):
+          self.line[i].empty()
+      self.line[:] = []
+      for i in range(11):
+          nn = pygame.sprite.Group()
+          self.line.append(nn)
+      ss = 0
+      for x, y in XY:
+          # 实例化砖块类对象
+          if ss//dril1.levelwin:
+              self.brick = Brick(random.choice(['red', 'blue', 'green', 'yellow', 'brown', 'white', 'crystal', 'air']))
+          else:
+              self.brick = Brick(random.choice(['red', 'blue', 'green', 'yellow', 'brown', 'white', 'crystal']))
+          # 生砖块的位置
+          self.brick.rect.left, self.brick.rect.top = 3 + x * 50, 250 + y * 50  # 每循环一次自动将动画添加到精灵组（下同）
+          self.brickGroup.add(self.brick)
+          self.line[x].add(self.brick)
+          ss+=1
 class Drillers(pygame.sprite.Sprite):
     life=3
-    level=0
+    level=0#depth
     air=100
     levelwin=1
     image = pygame.image.load(foo('drill'))
@@ -257,43 +251,41 @@ class Drillers(pygame.sprite.Sprite):
         self.rect.top+=speedy
     def fall(self):#修改
         '''第二次尝试'''
-        xx=(self.rect.left-3)//50
+        xx=(self.rect.left-2)//50#身体左侧所在的列位
         collide_list = pygame.sprite.spritecollide(self, map1.line[xx], False)
-        '''原
-        collide_list = pygame.sprite.spritecollide(self, map1.brickGroup, False)'''
         self.speed=2
         for sp in collide_list:
             if self.rect.top+38<=sp.rect.top<=self.rect.top+41 :#下接触了
-                if sp.rect.left - 32 < self.rect.left < sp.rect.left + 50:
+                #if sp.rect.left - 32 < self.rect.left < sp.rect.left + 50:
                     self.rect.top=sp.rect.top-39
                     self.speed=0
                     return 0
-        xx2 = (self.rect.left +29) // 50
+        xx2 = (self.rect.left +27) // 50#身体右侧所在的列位
         if xx2!=xx:
             collide_list2 = pygame.sprite.spritecollide(self, map1.line[xx2], False)
             for sp in collide_list2:
                 if self.rect.top + 38 <= sp.rect.top <= self.rect.top + 41:  # 下接触了
-                    if sp.rect.left - 32 < self.rect.left < sp.rect.left + 50:
-                        self.rect.top = sp.rect.top - 39
+                        self.rect.top = sp.rect.top - 39#矫正位置
                         self.speed = 0
                         return 0
-            #else:
-             #   if sp.rect.left + 50 > self.rect.left > sp.rect.left + 25: self.rect.left = sp.rect.left  # 排左
-              #  if sp.rect.left < self.rect.left + 32 < sp.rect.left + 25: self.rect.left = sp.rect.left - 32  # 排右
-                #self.rect.top=sp.rect.top-39
-        if self.rect.top + self.level >= 2210:return 0
         self.rect.top+=self.speed
     def update(self):
         self.fall()
-        #if self.rect.top+self.level>=2210:print("WIN")
-        if self.score>=100:
+        if (self.level + 100) >= self.levelwin * 200 + 800:  # 550+250
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        if (self.level+self.rect.top+40)>=map1.length+250:#self.levelwin*200+800:#550+250升级
             self.levelwin+=1
-            self.score-=100
+            self.score+=50
+            print(self.level)
+            #self.air=???
+            '''尝试'''
+            map1.changemap()
+            dril1.rect.left, dril1.rect.top = 250, 200
+            dril1.level = 0
+
 
     #def dill(self)#棕色的氧气减20%
     #def collect(self):#air+20%
-
-
 
 def mergebrick(tics):
     ii=0
@@ -341,7 +333,10 @@ def mergebrick(tics):
                             if jiaocha.fallingsingle==0:continue
                             spp.aadd(jiaocha)
         if spp.num >= 4:  # 大于4
-            # if spp.statu == 0: continue
+            '''是否移动过'''
+            if spp.statu == 0:
+                ii+=1
+                continue
             ''''''
             if spp.color == "crystal" :
                 for bbb in spp.mapn:
@@ -362,25 +357,6 @@ def mergebrick(tics):
             meltgroup.pop(ii)
         ii+=1
 
-'''
-def mergedisappear(megroup,i):
-       if spp.num >= 4:  # 大于4
-           # if spp.statu == 0: continue
-           if spp.color == "crystal" :
-               for bbb in spp.mapn:
-                   if bbb.mel!=1:
-                       if bbb.stoptime!=0:pass
-                       else:
-                           bbb.stoptime=2.5
-                           print("透明色数量：", spp.num)
-           for yichu in spp.mapn:
-               map1.brickGroup.remove(yichu)
-           dril1.score += spp.num
-           spp.mapn.empty  # 清空
-           print(dril1.score)
-           meltgroup.pop(i)
-
-'''
 def drillbrick(sp):
     if sp.life > 0:
         sp.life -= 1
@@ -394,6 +370,7 @@ def drillbrick(sp):
                     dril1.score += gro.num
                     if sp.color == "air":
                         dril1.air += 20 * gro.num
+                        if dril1.air>100:dril1.air=100
                     for yichu in gro.mapn:
                         map1.brickGroup.remove(yichu)
                         x = yichu.rect.left // 50
@@ -405,6 +382,7 @@ def drillbrick(sp):
                 ssss += 1
         if sp.color=="air":
             dril1.air+=20
+            if dril1.air>100:dril1.air=100
         dril1.score+=1
         map1.brickGroup.remove(sp)
         x = sp.rect.left//50
@@ -422,6 +400,8 @@ def levelup():
         dril1.rect.top += 2
         dril1.level -= 2
 
-map1=Map()
+
 dril1=Drillers()
+map1=Map()
+
 dril1.rect.left,dril1.rect.top=250,200
--- main.py
+++ main.py
@@ -9,61 +9,162 @@ spdy = 0
 tic=0
 kkk1,kkk2=0,0
 moveable=1
+backimage = pygame.image.load("th.jpg")
+rect = backimage.get_rect()
+rect2 = pygame.Rect(0,0,rect.width ,rect.height)
+rightimage=pygame.image.load("right.jpg")
 text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
 text_fmt2 = my_font.render("LIFE:" + str(dril1.life), 1, (255, 255, 255))
 text_fmt3 = my_font.render("LEVEL:" + str(dril1.levelwin), 1, (255, 255, 255))
 text_fmt4 = my_font.render("SCORE:" + str(dril1.score), 1, (255, 255, 255))
+text_fmtwin = my_font.render("YOU WIN", 5, (255, 255, 255))
+text_fmtlose = my_font.render("YOU LOSE", 5, (255, 255, 255))
+text_fmtpause = my_font.render("PAUSE", 2, (255, 255, 255))
 lifen=str(dril1.life)
+pause=0
 while True:
     '''TIME'''
-    timepassed = framerate.tick(60)
+    timepassed = framerate.tick(30)
     tic += timepassed / 1000
     screen.fill((0, 0, 0))
+    '''background'''
+    rect2.y =dril1.level+40
+    if rect2.y>900:rect2.y=dril1.level+40-900
+    screen.blit(backimage,(0,0),rect2)#背景图的移动
+    screen.blit(rightimage, (555, 0))
     '''DIE'''
     if dril1.air<=0:
         dril1.life-=1
         dril1.air=100
         text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
         text_fmt2 = my_font.render("LIFE:" + str(dril1.life), 1, (255, 255, 255))
+    '''输赢，暂未成功'''
     if dril1.life==0:
-        pygame.quit()
-        sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print(kkk1, "@@@", kkk2)
+                pygame.quit()
+                sys.exit()
+        screen.fill((0, 0, 0))
+        '''画砖块，人'''
+        map1.brickGroup.draw(screen)
+        screen.blit(dril1.image, dril1.rect)
+        '''右边信息栏'''
+        screen.blit(text_fmt, (600, 100))
+        screen.blit(text_fmt2, (600, 200))
+        screen.blit(text_fmt3, (600, 300))
+        screen.blit(text_fmt4, (600, 400))
+        screen.blit(text_fmtlose, (250, 250))
+        pygame.display.update()
+        continue
+    if dril1.levelwin>=10:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print(kkk1, "@@@", kkk2)
+                pygame.quit()
+                sys.exit()
+        screen.fill((0, 0, 0))
+        '''画砖块，人'''
+        map1.brickGroup.draw(screen)
+        screen.blit(dril1.image, dril1.rect)
+        '''右边信息栏'''
+        screen.blit(text_fmt, (600, 100))
+        screen.blit(text_fmt2, (600, 200))
+        screen.blit(text_fmt3, (600, 300))
+        screen.blit(text_fmt4, (600, 400))
+
+        screen.blit(text_fmtwin, (250, 250))
+        pygame.display.update()
+        continue
+
+    '''pause'''
+    if pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print(kkk1, "@@@", kkk2)
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                if 560<x<668 and 500<y<540:
+                    pause=0
+        screen.fill((0, 0, 0))
+        '''画砖块，人'''
+        map1.brickGroup.draw(screen)
+        screen.blit(dril1.image, dril1.rect)
+        '''右边信息栏'''
+        screen.blit(text_fmt, (600, 100))
+        screen.blit(text_fmt2, (600, 200))
+        screen.blit(text_fmt3, (600, 300))
+        screen.blit(text_fmt4, (600, 400))
+        screen.blit(text_fmtpause, (600, 500))
+        pygame.display.update()
+        continue
+
     '''driller移动+打砖块'''
-    collide_list2 = pygame.sprite.spritecollide(dril1, map1.brickGroup, False)
     key_pressed = pygame.key.get_pressed()
     spdx = 0  # 不动的时候x速度为0
     '''移动'''
+    x = (dril1.rect.left + 13) // 50  # 中间
+    collide_listm = pygame.sprite.spritecollide(dril1, map1.line[x], False)
+    if x<10:
+        rrr= x+1
+        collide_listr = pygame.sprite.spritecollide(dril1, map1.line[rrr], False)
+    else:
+        rrr=x
+        collide_listr=collide_listm
+    if x>=1:
+        lll=x-1
+        collide_listl = pygame.sprite.spritecollide(dril1, map1.line[lll], False)
+    else:
+        lll = x
+        collide_listl=collide_listm
     if key_pressed and moveable:
         if key_pressed[pygame.K_RIGHT]:
             spdx = 2
-            for sp in collide_list2:
-                if dril1.rect.left + 16 < sp.rect.left <= dril1.rect.left + 33:  # 右边挡住了
-                    if sp.rect.top < dril1.rect.top + 30: spdx = 0  # 腰上边的
+            if x!=rrr:
+                for sp in collide_listr:
+                    if dril1.rect.left + 16 < sp.rect.left <= dril1.rect.left + 33:  # 右边挡住了
+                        if sp.rect.top < dril1.rect.top + 30: spdx = 0  # 腰上边的
             kkk1 += spdx
         if key_pressed[pygame.K_LEFT]:
             spdx = -2
-            for sp in collide_list2:
-                if dril1.rect.left + 16 > sp.rect.left + 51 >= dril1.rect.left:
-                    if sp.rect.top < dril1.rect.top + 30: spdx = 0  # 腰上边的
+            if x!=lll:
+                for sp in collide_listl:
+                    if dril1.rect.left + 16 > sp.rect.left + 51 >= dril1.rect.left:
+                        if sp.rect.top < dril1.rect.top + 30: spdx = 0  # 腰上边的
             kkk2 += spdx
     '''打砖块'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(kkk1,"@@@" ,kkk2)
+            print(kkk1, "@@@", kkk2)
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:#加了分数air显示
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            if 560 < x < 668 and 500 < y < 540:
+                pause = 1
+        if event.type == pygame.KEYDOWN:  # 加了分数air显示
             # 键盘有按下？
             if event.key == pygame.K_UP:
-                if dril1.rect.top > 550: spdy = -6
+                if dril1.rect.top > 550:
+                    spdy = -6
                 else:
-                    for sp in collide_list2:
+                    for sp in collide_listm:
                         if sp.rect.top > dril1.rect.top + 35 and dril1.rect.top < sp.rect.top: spdy = -6
+                    xx=dril1.rect.left
+                    if xx<x*50+3:#左边超出
+                        for sp in collide_listl:
+                            if sp.rect.top > dril1.rect.top + 35 and dril1.rect.top < sp.rect.top: spdy = -6
+                    elif xx+32>x*50+53:#右边超出
+                        for sp in collide_listr:
+                            if sp.rect.top > dril1.rect.top + 35 and dril1.rect.top < sp.rect.top: spdy = -6
+
             if event.key == pygame.K_DOWN:
-                for sp in collide_list2:
-                    if sp.rect.left<dril1.rect.left+16<sp.rect.left+50:
+                for sp in collide_listm:
+                    if sp.rect.top <= dril1.rect.top + 40 <= sp.rect.top + 10:
                         if sp.color == "crystal":
-                            if (sp.rect.top+dril1.level) % 50 == 0 and sp.stoptime == 0:
+                            if (sp.rect.top + dril1.level) % 50 == 0 and sp.stoptime == 0:
                                 sp.stoptime = 2.5  # 3秒后stoptime=-0.1
                                 break
                             else:
@@ -73,66 +174,85 @@ while True:
 
                         text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
 
-                        spdy=0
+                        spdy = 0
                         break
             if event.key == pygame.K_LEFT:
                 moveable = 1
-                for sp in collide_list2:
-                    if sp.rect.top<dril1.rect.top+10<sp.rect.top+50 and dril1.rect.left + 16 > sp.rect.left + 51 >= dril1.rect.left:
-                        moveable=0
-                        spdx=0
-                        if sp.life>0:
-                            sp.life-=1
-                        else:
-                            if sp.color == "crystal":
-                                if (sp.rect.top+dril1.level) % 50 == 0 and sp.stoptime == 0:
-                                    sp.stoptime = 2.5  # 3秒后stoptime=-0.1
-                                    break
-                                else:
-                                    break
-                            drillbrick(sp)
-                            text_fmt4 = my_font.render("SCORE:" + str(dril1.score), 1, (255, 255, 255))
-                            text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
+                if x!=0:
+                    for sp in collide_listl:
+                        if sp.rect.top < dril1.rect.top + 10 < sp.rect.top + 50 and dril1.rect.left + 16 > sp.rect.left + 51 >= dril1.rect.left:
+                            moveable = 0
+                            spdx = 0
+                            if sp.life > 0:
+                                sp.life -= 1
+                            else:
+                                if sp.color == "crystal":
+                                    if (sp.rect.top + dril1.level) % 50 == 0 and sp.stoptime == 0:
+                                        sp.stoptime = 2.5  # 3秒后stoptime=-0.1
+                                        break
+                                    else:
+                                        break
+                                drillbrick(sp)
+                                text_fmt4 = my_font.render("SCORE:" + str(dril1.score), 1, (255, 255, 255))
+                                text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
                         break
             if event.key == pygame.K_RIGHT:
                 moveable = 1
-                for sp in collide_list2:
-                    if sp.rect.top<dril1.rect.top+10<sp.rect.top+50 and dril1.rect.left + 16 < sp.rect.left <= dril1.rect.left + 33:
-                        moveable=0
-                        spdx=0
-                        if sp.life>0:
-                            sp.life-=1
-                        else:
-                            if sp.color == "crystal":
-                                if (sp.rect.top+dril1.level) % 50 == 0 and sp.stoptime == 0:
-                                    sp.stoptime = 2.5  # 3秒后stoptime=-0.1
-                                    break
-                                else:
-                                    break
-                            drillbrick(sp)
-                            text_fmt4 = my_font.render("SCORE:" + str(dril1.score), 1, (255, 255, 255))
-                            text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
-                        break
-    dril1.move(spdx,spdy)
-    if spdy<0:
-        spdy += 0.2 # 跳跃减速
-        for sp in collide_list2:
-            if sp.rect.top+51>=dril1.rect.top>=sp.rect.top+25:
-                spdy=0
+                if x!=10:
+                    for sp in collide_listr:
+                        if sp.rect.top < dril1.rect.top + 10 < sp.rect.top + 50 and dril1.rect.left + 16 < sp.rect.left <= dril1.rect.left + 33:
+                            moveable = 0
+                            spdx = 0
+                            if sp.life > 0:
+                                sp.life -= 1
+                            else:
+                                if sp.color == "crystal":
+                                    if (sp.rect.top + dril1.level) % 50 == 0 and sp.stoptime == 0:
+                                        sp.stoptime = 2.5  # 3秒后stoptime=-0.1
+                                        break
+                                    else:
+                                        break
+                                drillbrick(sp)
+                                text_fmt4 = my_font.render("SCORE:" + str(dril1.score), 1, (255, 255, 255))
+                                text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
+                            break
+
+    '''移动+跳跃减速'''
+    dril1.move(spdx, spdy)
+    if spdy < 0:
+        spdy += 0.2  # 跳跃减速
+        for sp in collide_listm:
+            if sp.rect.top + 51 >= dril1.rect.top >= sp.rect.top + 25:
+                spdy = 0
                 break
+        if xx < x * 50 + 3:  # 左边超出
+            for sp in collide_listr:
+                if sp.rect.top + 51 >= dril1.rect.top >= sp.rect.top + 25:
+                    spdy = 0
+                    break
+        elif xx + 32 > x * 50 + 53:  # 右边超出
+            for sp in collide_listr:
+                if sp.rect.top + 51 >= dril1.rect.top >= sp.rect.top + 25:
+                    spdy = 0
+                    break
+
     '''砖块融合消除'''
     mergebrick(tic)
     map1.brickGroup.update(tic)
+    '''画砖块，人'''
     map1.brickGroup.draw(screen)
     screen.blit(dril1.image, dril1.rect)
     '''右边信息栏'''
-    screen.blit(text_fmt, (560, 100))
-    screen.blit(text_fmt2, (560, 200))
-    screen.blit(text_fmt3, (560, 300))
-    screen.blit(text_fmt4, (560, 400))
+    screen.blit(text_fmt, (600, 100))
+    screen.blit(text_fmt2, (600, 200))
+    screen.blit(text_fmt3, (600, 300))
+    screen.blit(text_fmt4, (600, 400))
+    screen.blit(text_fmtpause, (600, 500))
     '''level'''
     if dril1.rect.top<560: dril1.update()
+    '''深度'''
     levelup()
+    '''刷新'''
     pygame.display.update()
     '''AIR'''
     if tic >= 0.2:
@@ -146,5 +266,3 @@ while True:
         dril1.air-=1
         text_fmt = my_font.render("AIR:" + str(dril1.air), 1, (255, 255, 255))
         print(dril1.air)
-
-
