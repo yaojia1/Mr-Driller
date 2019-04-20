a9e7643475595436212689fe013ed5a7d1b3852a
yaojia1 <288167@supinfo.com>
2019-04-20 14:06:55 +0800

    for zhu baby

---
+++ pages.py
@@ -0,0 +1,18 @@
+#for祝宝宝
+from elements import *
+def startpage(screen,x,y,event):
+    pass
+    '''bilt，画2个按钮在screen上一个开始游戏，一个是help游戏讲解'''
+    '''x,y是鼠标坐标，event是鼠标事件'''
+    '''if 点击help:
+         就新画一个界面
+         写上操作方法和一个返回按钮返回上个界面
+    elif点击开始游戏:
+         return 0 开始游戏'''
+    '''你可以设置几个参数加到参数栏来判断点击状态，因为这是个游戏循环会不停的调用这个函数'''
+def endpage(screen,x,y,event):
+    pass
+    '''恭喜通关的文字之类的'''
+    '''输入姓名'''
+    '''一个提交姓名的按钮'''
+    '''点击提交后姓名分数写入排名文件，页面上显示通关排名'''
